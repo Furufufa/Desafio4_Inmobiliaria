71d359969dfb961df9e64af6e061c936539f4ed4
Solange Benítez Saavedra <solange.benitez@gmail.com>
2024-03-24 06:14:31 -0300

    Agrega a GitHub el desafío 4, con sus respectivos archivos

---
+++ Desafio_arreglos_objetos/assets/js/script.js
@@ -0,0 +1,147 @@
+
+const propiedades_venta = [
+    {
+        src:"https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
+        nombre: "Apartamento de lujo en zona exclusiva",
+        descripcion:"Este apartamento de lujo está ubicado en una exclusiva zona residencial",
+        ubicacion:" 123 Luxury Lane,Prestige Suburb, CA 45678",
+        habitaciones: 4,
+        baños: 4,
+        costo: 5000,
+        smoke: false,
+        pets: false
+    },
+    {
+        src:"https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg",
+        nombre: "Apartamento acogedor en la montaña",
+        descripcion:"Este apartamento acogedor está situado en lo alto de una montaña con impresionantes vistas",
+        ubicacion:"789 Mountain Road, Summit Peaks, CA 23456",
+        habitaciones:2,
+        baños: 1,
+        costo:1200,
+        smoke:true,
+        pets:true        
+    },
+    {
+        src:"https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg",
+        nombre: "Penthouse de lujo con terraza panorámica",
+        descripcion:"Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares",
+        ubicacion:"567 Skyline Avenue, Skyview City, CA 56789",
+        habitaciones: 3,
+        baños: 3,
+        costo: 4500,
+        smoke:false,
+        pets:true
+    },
+    {
+        src:"https://static.tokkobroker.com/pictures/20767145484430375441100344251233814733548840366038290261155684921592209671599.jpg",
+        nombre: "Departamento Duplex de lujo en sector exclusivo",
+        descripcion: "Ubicado en sector cercano a carretera, colegios, malls y supermercados",
+        ubicacion: "Avenida el Rodeo 14652, Lo Barnechea",
+        habitaciones: 4,
+        baños: 3,
+        costo: 2500,
+        smoke: false,
+        pets: true,
+    },
+    {
+        src:"./assets/img/mediterranea.jpg",
+        nombre: "Casa Estilo Mediterranea exclusiva",
+        descripcion: "Vivienda que cuenta con eficiencia luminica, dispone de piscina, cine y gimnasio",
+        ubicacion: "Avenida werner 1248, Hornitos, Antofagasta",
+        habitaciones: 6,
+        baños: 4,
+        costo: 8000,
+        smoke: true,
+        pets: true,
+    },
+    {
+        src: "./assets/img/casaBosque.jpg",
+        nombre: "Casa cercana a Lago Llanquihue",
+        descripcion: "Hermosa casa ubicada en el bosque, con eficiencia energetica",
+        ubicacion: "Camino al Lago, parcela 9, Puerto Varas",
+        habitaciones: 4,
+        baños: 3,
+        costo: 6000,
+        smoke: false,
+        pets: true,
+    },
+    
+    
+];
+
+
+const propiedades_alquiler = [
+    {
+        src:"https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60",
+        nombre: "Apartamento en el centro de la ciudad",
+        descripcion:"Este apartamento de 2 habitaciones está ubicado en el corazón de la ciudad, cerca de todo.",
+        ubicacion:"123 Main Street, Anytown, CA 91234",
+        habitaciones: 2,
+        baños: 2,
+        costo: 2000,
+        smoke: false,
+        pets: true,
+    },
+    {
+        src:"https://images.unsplash.com/photo-1669071192880-0a94316e6e09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+        nombre: "Apartamento luminoso con vista al mar",
+        descripcion:"Este hermoso apartamento ofrece una vista impresionante al mar",
+        ubicacion:"456 Ocean Avenue, Seaside Town, CA 56789",
+        habitaciones: 3,
+        baños: 3,
+        costo: 2500,
+        smoke: true,
+        pets: true,
+
+    },
+    {
+        src:"https://images.unsplash.com/photo-1567496898669-ee935f5f647a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvbmRvfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60",
+        nombre: "Condominio moderno en zona residencial",
+        descripcion:"Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares",
+        ubicacion:"123 Main Street, Anytown, CA 912349",
+        habitaciones: 2,
+        baños: 2,
+        costo: 2200,
+        smoke: false,
+        pets: false,
+
+    },
+    {
+        src:"./assets/img/CasaPlaya.jpg",
+        nombre: "Casa de Playa en sector privado",
+        descripcion:"Moderna y amplia casa instalada en exclusiva playa privada, vista privilegiada",
+        ubicacion:"Camino a Tomé 9002, Condominio Puerta del Sol, casa 7, Tomé",
+        habitaciones: 5,
+        baños: 4,
+        costo: 4200,
+        smoke: true,
+        pets: true,
+
+    },
+    {
+        src:"./assets/img/penthouse.jpg",
+        nombre: "Penthouse con vista panorámica a la gran ciudad",
+        descripcion:"Exclusivo Departamento Ubicado sobre una colina, con vista panorámica",
+        ubicacion:"Camino del Alba 15678, La Dehesa",
+        habitaciones: 2,
+        baños: 4,
+        costo: 5000,
+        smoke:true,
+        pets: false,
+
+    },
+    {
+        src:"./assets/img/CasaEcológica.jpeg",
+        nombre: "Casa Autosustentable altamente eficiente",
+        descripcion:"Casa autosustentable, con paneles solares y sistema de agua propia",
+        ubicacion:"Las Paivas 798, Huentelauquen, Coquimbo",
+        habitaciones: 4,
+        baños: 2,
+        costo:4200,
+        smoke: true,
+        pets: true,
+
+    }
+
+]
